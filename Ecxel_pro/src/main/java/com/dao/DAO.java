74b8e8988b6a1da2253abf07f18d3a796d3b5f53
milan.balar <milan.balar@staging.abcprocure.com>
2020-11-23 18:21:14 +0530

    done

--- Ecxel_pro/src/main/java/com/dao/DAO.java
+++ Ecxel_pro/src/main/java/com/dao/DAO.java
@@ -1,6 +1,12 @@
 package com.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
+
+import javax.servlet.ServletRequest;
 
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +23,33 @@ public class DAO {
 
 	public void setHt(HibernateTemplate ht) {
 		this.ht = ht;
+	}
+
+	public static int insertData(ServletRequest request) throws ClassNotFoundException, SQLException {
+		int column = (Integer) request.getAttribute("totalColumn");
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		Connection con = DriverManager.getConnection("jdbc:sqlserver://192.168.100.55:1440;databaseName=eTest", "milan",
+				"F~?'7{e{");
+		PreparedStatement ps = con.prepareStatement("insert into MobileInfo values(" + getColumn(null) + ")");
+
+		for (int i = 1; i <= column; i++) {
+
+			ps.setString(i, (String) request.getAttribute("cell"));
+		}
+
+		int m = ps.executeUpdate();
+		return m;
+	}
+
+	private static String getColumn(ServletRequest request) {
+		String a = null;
+		int column = (Integer) request.getAttribute("totalColumn");
+		for (int i = 0; i < column - 1; i++) {
+
+			a += "?,";
+		}
+		a += "?";
+		return a;
 	}
 
 	@Transactional
--- Ecxel_pro/src/main/java/com/journaldev/spring/controller/FileUploadController.java
+++ Ecxel_pro/src/main/java/com/journaldev/spring/controller/FileUploadController.java
@@ -1,19 +1,23 @@
 package com.journaldev.spring.controller;
 
-import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Iterator;
-import java.util.List;
 
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,7 +25,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.dao.DAO;
-import com.journaldev.spring.model.MobileInfo;
 
 /**
  * Handles requests for the application file upload requests
@@ -29,14 +32,15 @@ import com.journaldev.spring.model.MobileInfo;
 @Controller
 public class FileUploadController {
 
-	// private static final Logger logger =
-	// LoggerFactory.getLogger(FileUploadController.class);
-
 	@Autowired
 	DAO dao;
 
 	@RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
-	public @ResponseBody String uploadFileHandler(@RequestParam("file") MultipartFile file) throws IOException {
+	public @ResponseBody String uploadFileHandler(@RequestParam("file") MultipartFile file, Model m)
+			throws IOException, ClassNotFoundException, SQLException {
+
+		Cell cell = null;
+		ByteArrayInputStream byteArrayInputStream = null;
 		System.out.println(file);
 		if (!file.isEmpty()) {
 
@@ -51,78 +55,169 @@ public class FileUploadController {
 
 			FileInputStream fis = new FileInputStream(convertedFile); // obtaining bytes from the file
 
-			// creating Workbook instance that refers to .xlsx file
 			XSSFWorkbook wb = new XSSFWorkbook(fis);
 
 			XSSFSheet sheet = wb.getSheetAt(0); // creating a Sheet object to retrieve object
 			Iterator<Row> itr = sheet.iterator(); // iterating over excel file
 
-			List<MobileInfo> mobileList = new ArrayList<MobileInfo>();
+			Row row = itr.next();
+			int column = row.getRowNum();
+			int totalColumn = column + 1;
 
-			MobileInfo mobileInfo;
+			System.out.println("total column is " + totalColumn);
 
-			while (itr.hasNext()) {
+			m.addAttribute("totalColumn", totalColumn);
 
-				Row row = itr.next();
+			// logic code
+			int noOfSheet = wb.getNumberOfSheets();
 
-				if (row.getRowNum() > 0) {
-					mobileInfo = new MobileInfo();
-					mobileInfo.setId((int) row.getCell(0).getNumericCellValue());
-					mobileInfo.setName(row.getCell(1).getStringCellValue());
-					mobileInfo.setAmount((int) row.getCell(2).getNumericCellValue());
-					mobileInfo.setTotal((int) row.getCell(3).getNumericCellValue());
+			System.out.println("number of sheets :" + noOfSheet);
+			for (int i = 0; i < noOfSheet; i++) {
+				sheet = wb.getSheet(wb.getSheetName(i));
+				if (sheet != null) {
+					row = sheet.getRow(0);
 
-					mobileList.add(mobileInfo);
+					if (row != null) {
+						Connection conn = null;
+						Statement stmt = null;
+						int lastCellNumebr = row.getLastCellNum();
+						StringBuilder createQuery2 = new StringBuilder();
+						StringBuilder createQuery3 = new StringBuilder();
+						Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+						conn = DriverManager.getConnection("jdbc:sqlserver://192.168.100.55:1440;databaseName=eTest",
+								"milan", "F~?'7{e{");
+						System.out.println("Connected database & delete table successfully...");
+						stmt = conn.createStatement();
+						String sql = "DROP TABLE ABC_m";
+						stmt.executeUpdate(sql);
+						System.out.println("existing table deleted.... ");
+						createQuery2.append("CREATE TABLE ABC_m ").append(" ( ");
+						createQuery2.append("id INT PRIMARY KEY IDENTITY (1, 1)");
+						for (int cellNo = 0; cellNo < lastCellNumebr; cellNo++) {
+							try {
+								cell = row.getCell(cellNo);
+								if (cell != null) {
+									createQuery2.append(", ").append(
+											cell.getStringCellValue().replace(".", "").replaceAll("\\s", "").trim())
+											.append(" VARCHAR(255)");
+									System.out.println("  cellNo  :" + cellNo + " cell.getStringCellValue() "
+											+ cell.getStringCellValue());
+								}
+							} catch (Exception ex) {
+								ex.printStackTrace();
+							}
+						}
+						createQuery2.append(")");
+						System.out.println(createQuery2.toString());
 
+						Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+						conn = DriverManager.getConnection("jdbc:sqlserver://192.168.100.55:1440;databaseName=eTest",
+								"milan", "F~?'7{e{");
+						System.out.println("Connected database successfully...");
+						stmt = conn.createStatement();
+
+						stmt.executeUpdate(createQuery2.toString());
+
+						conn = DriverManager.getConnection("jdbc:sqlserver://192.168.100.55:1440;databaseName=eTest",
+								"milan", "F~?'7{e{");
+						String sql2 = "insert into ABC_m values(" + getQuestion1(sheet) + ")";
+						PreparedStatement ps = conn.prepareStatement(sql2);
+
+						Row row2 = sheet.getRow(0);
+						int n = row2.getLastCellNum();
+						String t = "";
+						int batchSize = 20;
+
+						Iterator<Row> rows = sheet.iterator();
+						rows.next();
+						int count = 0;
+						while (rows.hasNext()) {
+							Row nextRow = rows.next();
+							Iterator<Cell> cellIterator = nextRow.cellIterator();
+
+							int temp = 1;
+							while (cellIterator.hasNext()) {
+								Cell nextCell = cellIterator.next();
+								if (nextCell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+									ps.setInt(temp++, (int) nextCell.getNumericCellValue());
+								} else {
+									ps.setString(temp++, nextCell.getStringCellValue());
+								}
+							}
+							ps.addBatch();
+
+							if (count % batchSize == 0) {
+								ps.executeBatch();
+							}
+
+						}
+						ps.executeBatch();
+
+					}
+
+					/*
+					 * for (int cellNo = 1; cellNo <= lastCellNumebr; cellNo++) { try {
+					 * System.out.println("cellNo is" + cellNo + "  cell.getStringCellValue()" +
+					 * cell.getStringCellValue()); cell = row.getCell(cellNo); if (cell != null) {
+					 * 
+					 * ps.setString(cellNo, cell.getStringCellValue());
+					 * 
+					 * } } catch (Exception ex) { ex.printStackTrace(); }
+					 */
 				}
+
+				System.out.println("sucess---data");
+
 			}
 
-			dao.save(mobileList);
-
-			System.out.println(mobileList.toString());
-			return "sucess";
-
-		} else {
-			return "You failed to upload  because the file was empty.";
 		}
+
+// end
+		return "sucess";
+
+	}
+
+	private String getQuestion1(XSSFSheet sheet) {
+		Row row = sheet.getRow(0);
+		int n = row.getLastCellNum();
+		String t = "";
+		for (int i = 0; i < n - 1; i++) {
+			t += "?,";
+		}
+
+		return t + "?";
 	}
 
 	/**
 	 * Upload multiple file using Spring Controller
 	 */
-	@RequestMapping(value = "/uploadMultipleFile", method = RequestMethod.POST)
-	public @ResponseBody String uploadMultipleFileHandler(@RequestParam("name") String[] names,
-			@RequestParam("file") MultipartFile[] files) {
 
-		if (files.length != names.length)
-			return "Mandatory information missing";
-
-		String message = "";
-		for (int i = 0; i < files.length; i++) {
-			MultipartFile file = files[i];
-			String name = names[i];
-			try {
-				byte[] bytes = file.getBytes();
-
-				// Creating the directory to store file
-				String rootPath = System.getProperty("catalina.home");
-				File dir = new File(rootPath + File.separator + "tmpFiles");
-				if (!dir.exists())
-					dir.mkdirs();
-
-				// Create the file on server
-				File serverFile = new File(dir.getAbsolutePath() + File.separator + name);
-				BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
-				stream.write(bytes);
-				stream.close();
-
-				System.out.println("Server File Location=" + serverFile.getAbsolutePath());
-
-				message = message + "You successfully uploaded file=" + name + "<br />";
-			} catch (Exception e) {
-				return "You failed to upload " + name + " => " + e.getMessage();
-			}
-		}
-		return message;
-	}
+	/*
+	 * @RequestMapping(value = "/uploadMultipleFile", method = RequestMethod.POST)
+	 * public @ResponseBody String uploadMultipleFileHandler(@RequestParam("name")
+	 * String[] names,
+	 * 
+	 * @RequestParam("file") MultipartFile[] files) {
+	 * 
+	 * if (files.length != names.length) return "Mandatory information missing";
+	 * 
+	 * String message = ""; for (int i = 0; i < files.length; i++) { MultipartFile
+	 * file = files[i]; String name = names[i]; try { byte[] bytes =
+	 * file.getBytes();
+	 * 
+	 * // Creating the directory to store file String rootPath =
+	 * System.getProperty("catalina.home"); File dir = new File(rootPath +
+	 * File.separator + "tmpFiles"); if (!dir.exists()) dir.mkdirs();
+	 * 
+	 * // Create the file on server File serverFile = new File(dir.getAbsolutePath()
+	 * + File.separator + name); BufferedOutputStream stream = new
+	 * BufferedOutputStream(new FileOutputStream(serverFile)); stream.write(bytes);
+	 * stream.close();
+	 * 
+	 * System.out.println("Server File Location=" + serverFile.getAbsolutePath());
+	 * 
+	 * message = message + "You successfully uploaded file=" + name + "<br />"; }
+	 * catch (Exception e) { return "You failed to upload " + name + " => " +
+	 * e.getMessage(); } } return message; }
+	 */
 }
